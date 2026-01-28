1dca8e9196ab6fe2a09f5e8d9f5cfee248f71b3b
supratim24 <supratimhldr6@gmail.com>
2026-01-29 00:23:32 +0530

    feat: Rate limiting

---
+++ src/middleware/rateLimit.js
@@ -0,0 +1,29 @@
+const MongoStore = require('rate-limit-mongo');
+
+exports.initLimiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: 5,
+    store: new MongoStore({
+        uri: process.env.MONGO_URI,
+        collectionName: 'rateLimits',
+        expireTimeMs: 60 * 1000 // 1 minute
+    }),
+    keyGenerator: (req) => req.cookies.vote_token || softHash(req),
+    message: {
+        message: 'Too many init requests from this user, please try again later.'
+    }
+});
+
+exports.voteLimiter = rateLimit({
+    windowMs: 60 * 1000, // 1 minute
+    max: 3,
+    store: new MongoStore({
+        uri: process.env.MONGO_URI,
+        collectionName: 'rateLimits',
+        expireTimeMs: 60 * 1000 // 1 minute
+    }),
+    keyGenerator: (req) => req.cookies.vote_token || softHash(req),
+    message: {
+        message: 'Too many vote requests from this user, please try again later.'
+    }
+});
--- src/routes/poll.routes.js
+++ src/routes/poll.routes.js
@@ -1,8 +1,9 @@
 const router = require('express').Router();
 const controller = require('../controllers/poll.controller');
+const { initLimiter, voteLimiter } = require('../middleware/rateLimit');
 
-router.post("/vote", controller.votePoll);
+router.post("/vote", voteLimiter, controller.votePoll);
 router.get("/result", controller.pollResults);
-router.post("/init", controller.initPoll);
+router.post("/init", initLimiter, controller.initPoll);
 
 module.exports = router;
